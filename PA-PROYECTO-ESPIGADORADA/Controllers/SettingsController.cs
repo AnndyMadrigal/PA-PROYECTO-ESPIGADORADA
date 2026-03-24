e01a4506dcada3bcf430f92a0e9b7b357c0307b6
Camila Alfaro <camilaalfa20@gmail.com>
2026-03-24 09:37:33 -0600

    ID-06
    Se implementa módulo de configuración general utilizando tabla settings.
    Incluye vista, controlador y edición de parámetros globales., implementada con persistencia y auditoría.

---
+++ PA-PROYECTO-ESPIGADORADA/Controllers/SettingsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using PA_PROYECTO_ESPIGADORADA.EntityFramework;
+using PA_PROYECTO_ESPIGADORADA.Filters;
+using PA_PROYECTO_ESPIGADORADA.Models;
+
+namespace PA_PROYECTO_ESPIGADORADA.Controllers
+{
+    [ActiveSession]
+    [AllowRoles(1, 2)]
+    public class SettingsController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            using (var context = new Espiga_DBEntities())
+            {
+                var setting = context.settings.FirstOrDefault();
+
+                var model = new SettingsModel();
+
+                if (setting != null)
+                {
+                    model.SettingId = setting.setting_id;
+                    model.CompanyName = setting.company_name;
+                    model.CurrencySymbol = setting.currency_symbol;
+                    model.SupportEmail = setting.support_email;
+                    model.LowStockThreshold = setting.low_stock_threshold;
+                }
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(SettingsModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            string currentUser = GetAuditUser();
+
+            using (var context = new Espiga_DBEntities())
+            {
+                var setting = context.settings.FirstOrDefault();
+
+                if (setting == null)
+                {
+                    setting = new settings
+                    {
+                        company_name = model.CompanyName,
+                        currency_symbol = model.CurrencySymbol,
+                        support_email = model.SupportEmail,
+                        low_stock_threshold = model.LowStockThreshold,
+                        created_by = currentUser
+                    };
+
+                    context.settings.Add(setting);
+                }
+                else
+                {
+                    setting.company_name = model.CompanyName;
+                    setting.currency_symbol = model.CurrencySymbol;
+                    setting.support_email = model.SupportEmail;
+                    setting.low_stock_threshold = model.LowStockThreshold;
+                    setting.modified_by = currentUser;
+                }
+
+                context.SaveChanges();
+            }
+
+            TempData["Mensaje"] = "La configuración general se guardó correctamente.";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PA-PROYECTO-ESPIGADORADA/Models/SettingsModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PA_PROYECTO_ESPIGADORADA.Models
+{
+    public class SettingsModel
+    {
+        public int SettingId { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la empresa es obligatorio.")]
+        [Display(Name = "Nombre de la empresa")]
+        public string CompanyName { get; set; }
+
+        [Required(ErrorMessage = "El símbolo de moneda es obligatorio.")]
+        [StringLength(5, ErrorMessage = "Máximo 5 caracteres.")]
+        [Display(Name = "Símbolo de moneda")]
+        public string CurrencySymbol { get; set; }
+
+        [Required(ErrorMessage = "El correo de soporte es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Ingrese un correo válido.")]
+        [Display(Name = "Correo de soporte")]
+        public string SupportEmail { get; set; }
+
+        [Range(0, 100000, ErrorMessage = "Ingrese un valor válido.")]
+        [Display(Name = "Umbral de stock bajo")]
+        public int LowStockThreshold { get; set; }
+    }
+}
