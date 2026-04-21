3807ad8c0d1205ce5f0f152f7c6fced0af514aeb
falfaro20310-art <falfaro20310@ufide.ac.cr>
2026-04-21 01:22:01 -0600

    Req 14

---
+++ PA-PROYECTO-ESPIGADORADA/Models/OrderViewModel.cs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PA_PROYECTO_ESPIGADORADA.Models
+{
+    public class OrderDetailViewModel
+    {
+        [Required]
+        public int product_id { get; set; }
+
+        [Required]
+        public int quantity { get; set; }
+
+        [Required]
+        public decimal unit_cost { get; set; }
+    }
+
+    public class OrderViewModel
+    {
+        [Required(ErrorMessage = "Seleccione un proveedor")]
+        public int supplier_id { get; set; }
+
+        public List<OrderDetailViewModel> details { get; set; } = new List<OrderDetailViewModel>();
+    }
+}
+
